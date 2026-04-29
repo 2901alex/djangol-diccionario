5c70076e10ea28365901f341a10c20077b7748d2
2901alexander <99703737+2901alexander@users.noreply.github.com>
2026-04-28 22:35:29 -0500

    subiendo algunos avances del entorno como .env para seguridad

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,10 @@
 
 from pathlib import Path
+import os
+import environ
+
+env=eniron.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,10 +14,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#i#c-n(uec2@u@b0&h5i9juz=z&+zhik%k(($f)=-3dbj)ej8s'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
@@ -45,7 +50,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
